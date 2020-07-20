0d64e68aa4451a92d02e602c35b67b0e17a3bb5d
Flavien <flavien.murail@gmail.com>
2020-07-20 17:32:46 +0200

    Started to worked on the catalog artists page

--- site_pypo/site_pypo/urls.py
+++ site_pypo/site_pypo/urls.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
+from django.views.generic import TemplateView
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
+
 
 urlpatterns = [
     url(r'^django-admin/', admin.site.urls),
@@ -15,7 +18,8 @@ urlpatterns = [
     url(r'^documents/', include(wagtaildocs_urls)),
 
     url(r'^search/$', search_views.search, name='search'),
-
+    url(r"^404/", TemplateView.as_view(template_name='404.html')),
+    url(r"^500/", TemplateView.as_view(template_name='500.html')),
 ]
 
 
@@ -26,6 +30,7 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
