239b9445fdd0109fc8a2cdf37d7467d48b5e1ca7
Flavien <flavien.murail@gmail.com>
2020-07-21 17:52:28 +0200

    Added some tests to the Blog models

---
+++ site_pypo/blog/tests/tests_models.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+"""
+Author: [Nastyflav](https://github.com/Nastyflav) 2020-07-21
+Licence: `GNU GPL v3` GNU GPL v3: http://www.gnu.org/licenses/
+
+"""
+
+from wagtail.tests.utils import WagtailPageTests
+from wagtail.tests.utils.form_data import nested_form_data, streamfield
+
+from blog.models import BlogIndexPage, BlogPage
+from home.models import HomePage
+
+
+class TestArtistsModels(WagtailPageTests):
+    """To test the blog app models"""
+    def test_can_create_an_artist_page(self):
+        """Can create a page under the BlogIndexPage"""
+        self.assertCanCreateAt(BlogIndexPage, BlogPage)
+
+    def test_cant_create_under_artist_page(self):
+        """Cannot create a page under BlogPage"""
+        self.assertCanNotCreateAt(BlogPage, HomePage)
+
+    def test_can_create_under_home_page(self):
+        """Can create an BlogIndexPage under a HomePage"""
+        self.assertCanCreateAt(HomePage, BlogIndexPage)
+
+    def test_content_page_parent_pages(self):
+        """BlogPage can only be created under an BlogIndexPage"""
+        self.assertAllowedParentPageTypes(BlogPage, {BlogIndexPage})
+        """BlogIndexPage can only be created under a Home Page"""
+        self.assertAllowedParentPageTypes(BlogIndexPage, {HomePage})
