6d5c601aa60fe8a79d0920a044b0811b55f7a6c8
Flavien <flavien.murail@gmail.com>
2020-08-16 18:45:06 +0200

    Good work on the videos frontend integration, still a few things to fix

--- site_pypo/site_pypo/settings/base.py
+++ site_pypo/site_pypo/settings/base.py
@@ -163,3 +163,6 @@ WAGTAIL_SITE_NAME = "site_pypo"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+# Settings for responsive embed contents
+WAGTAILEMBEDS_RESPONSIVE_HTML = True
