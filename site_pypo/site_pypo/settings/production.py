eb0b24cb582aceea8a8ecd8c0bea20db140c1796
Flavien <flavien.murail@gmail.com>
2020-08-20 12:11:45 +0200

    Prepared the production settings file to Sentry

--- site_pypo/site_pypo/settings/production.py
+++ site_pypo/site_pypo/settings/production.py
@@ -1,5 +1,18 @@
 from .base import *
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+
+sentry_sdk.init(
+    dsn="https://6a3c0b450f574bbc8835ef6c51941301@o401185.ingest.sentry.io/5398165",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
 DEBUG = False
 
 try:
